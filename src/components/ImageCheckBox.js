2c423c6e8ed959d59a02ba04bdb22826438328b6
mdave16 <mdave3927@gmail.com>
2019-09-28 14:50:31 +0100

    add border around selected photos

--- src/components/ImageCheckBox.js
+++ src/components/ImageCheckBox.js
@@ -6,14 +6,25 @@ import './ImageCheckBox.css'
 class ImageCheckBox extends Component {
     constructor(props) {
         super(props);
-        this.state = {selected: false}
-        this.onSelect = this.onSelect.bind(this)
+        this.state = {selected: []}
+				this.onSelect = this.onSelect.bind(this)
+        this.selected = this.selected.bind(this)
     }
 
     onSelect(id) {
-        this.setState({selected: !this.state.selected})
+			const selected = this.state.selected
+			  if (selected.indexOf(id) > -1) {
+					 selected.splice(selected.indexOf(id), 1)
+				} else {
+					selected.push(id)
+				}
+				this.setState({selected: selected})
         this.props.onImageClick(id)
     }
+
+		selected(id) {
+			return this.state.selected.indexOf(id) > -1
+		}
 
     render() {
         const {images} = this.props
@@ -21,7 +32,7 @@ class ImageCheckBox extends Component {
             <React.Fragment>
                 {
                     images.map(({id, src, title}) => (
-                        <PhotoButton id={id} action={() => this.onSelect(id)} src={src} alt={title} />
+                        <PhotoButton selected={this.selected(id)} key={id} id={id} action={() => this.onSelect(id)} src={src} alt={title} />
                     ))
                 }
                 <div className="submit-button">
--- src/components/PhotoButton.js
+++ src/components/PhotoButton.js
@@ -2,5 +2,5 @@ import React from 'react'
 import './PhotoButton.css'
 
 export default function PhotoButton (props) {
-     return <img className='img-button' onClick={props.action} src={props.src} alt={props.alt}/>
+     return <img className={'img-button' + (props.selected ? ' selected': '') } onClick={props.action} src={props.src} alt={props.alt}/>
 }
