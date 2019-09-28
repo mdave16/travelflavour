8c9c689a7f3107fc1bd025e9e7cd68d36bea064d
mdave16 <mdave3927@gmail.com>
2019-09-28 11:34:06 +0100

    create a PhotoButton component to be the basic option

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import PhotoButton from './components/PhotoButton'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Main />
       <Footer />
+			<PhotoButton action={() => console.log('using PhotoButton')} src={'https://www.gravatar.com/avatar/cb7811690d295df620e222d0abd460ed'} alt='My gravatar'/>
     </div>
   );
 }
